de57f8f79b8bcef1e87b3a7c3f903ccddb8bfbc5
kanakainovasi <kanaka.inovasi@gmail.com>
2026-07-31 21:10:23 +0700

    fix: remove test page to restore original portfolio structure

--- app/page.js
+++
@@ -1,9 +0,0 @@
-﻿export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif", textAlign: "center" }}>
-      <h1>VA Simulator is Running</h1>
-      <p>Aplikasi berhasil dimuat dengan normal.</p>
-      <a href="/traffic" style={{ color: "blue", textDecoration: "underline" }}>Buka Dashboard Traffic</a>
-    </main>
-  );
-}
